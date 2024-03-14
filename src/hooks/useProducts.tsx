9ffb62a50735fff0598a625f084385e7b4e080c6
Alejandro Lete <alete8@gmail.com>
2024-03-14 11:47:08 -0300

    Initial commit

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -12,7 +12,7 @@ const useProducts = (): {
   const { products, setProducts, setShowAddModal, setShowEditModal, setShowDeleteModal } = useContext(ProductsContext)
 
   const addProduct = (product: Product): void => {
-    setProducts((prevState) => [...prevState, product])
+    setProducts((prevState: ListOfProducts) => [...prevState, product])
     setShowAddModal(false)
   }
 
